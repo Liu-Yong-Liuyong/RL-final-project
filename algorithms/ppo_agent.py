b0718477c089ff887534ee89fdca8dc825aeba1e
Liu-Yong-Liuyong <144796280+Liu-Yong-Liuyong@users.noreply.github.com>
2026-04-08 00:00:26 +0800

    Update ppo_agent.py

--- algorithms/ppo_agent.py
+++ algorithms/ppo_agent.py
@@ -1,18 +1,15 @@
 #sample ppo agent, that can be used in evaluate.py for loading the pretrained model
-import torch
+from stable_baselines3 import PPO
 
 class PPOAgent:
     def __init__(self, model):
         self.model = model
-        self.model.eval()
 
     @classmethod
-    def load(cls, model_path, model_class):
-        model = model_class()
-        model.load_state_dict(torch.load(model_path))
+    def load(cls, model_path):
+        model = PPO.load(model_path)
         return cls(model)
 
     def predict(self, obs):
-        with torch.no_grad():
-            action = self.model.act(obs)
+        action, _ = self.model.predict(obs, deterministic=True)
         return action
