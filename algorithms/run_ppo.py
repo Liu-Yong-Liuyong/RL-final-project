0d9e259fd8fcb62811c7b8fea8d4a945c1cd2d30
Liu-Yong-Liuyong <144796280+Liu-Yong-Liuyong@users.noreply.github.com>
2026-04-07 23:47:05 +0800

    Update run_ppo.py

--- algorithms/run_ppo.py
+++ algorithms/run_ppo.py
@@ -1,1 +1,12 @@
+'''
+#sample structure of ppo
+class PPOAgent:
+    def __init__(self, ...):
+        ...
 
+    def train(self, env, config):
+        ...
+
+    def predict(self, obs):
+        ...
+'''
