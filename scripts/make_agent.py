1cdd018fe829e4322197b2bfaee2e593a1c222ed
Liu-Yong-Liuyong <144796280+Liu-Yong-Liuyong@users.noreply.github.com>
2026-04-07 22:02:07 +0800

    Create make_agent.py

---
+++ scripts/make_agent.py
@@ -0,0 +1,27 @@
+from algorithms.baselines import (
+    RandomAgent,
+    FrozenLakeHeuristicAgent,
+    GridWorldHeuristicAgent,
+    CartPoleHeuristicAgent,
+)
+
+def make_agent(agent_name, env_name, env):
+    agent_name = agent_name.lower()
+    env_name = env_name.lower()
+
+    if agent_name == "random":
+        return RandomAgent(env.action_space)
+
+    if env_name == "frozenlake":
+        if agent_name == "heuristic":
+            return FrozenLakeHeuristicAgent(env.action_space)
+
+    if env_name == "gridworld":
+        if agent_name == "heuristic":
+            return GridWorldHeuristicAgent(env.action_space, goal_pos=env.goal_pos)
+
+    if env_name == "cartpole":
+        if agent_name == "heuristic":
+            return CartPoleHeuristicAgent(env.action_space)
+
+    raise ValueError(f"Unsupported agent '{agent_name}' for env '{env_name}'")
