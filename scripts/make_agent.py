22940d97dc08682e26697cfbf7c4b3aafbc232ba
Liu-Yong-Liuyong <144796280+Liu-Yong-Liuyong@users.noreply.github.com>
2026-04-07 23:58:26 +0800

    Update make_agent.py

--- scripts/make_agent.py
+++ scripts/make_agent.py
@@ -4,6 +4,7 @@ from algorithms.baselines import (
     GridWorldHeuristicAgent,
     CartPoleHeuristicAgent,
 )
+from algorithms.ppo_agent import PPOAgent 
 
 def make_agent(agent_name, env_name, env):
     agent_name = agent_name.lower()
@@ -23,5 +24,9 @@ def make_agent(agent_name, env_name, env):
     if env_name == "cartpole":
         if agent_name == "heuristic":
             return CartPoleHeuristicAgent(env.action_space)
+            
+    if agent_name == "ppo":
+        model_path = kwargs["model_path"]
+        return PPOAgent.load(model_path)
 
     raise ValueError(f"Unsupported agent '{agent_name}' for env '{env_name}'")
