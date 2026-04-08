872e03570462440b1c86f9a90e0bb7bcc07c7559
Liu-Yong-Liuyong <144796280+Liu-Yong-Liuyong@users.noreply.github.com>
2026-04-08 16:27:09 +0800

    Update make_agent.py

--- scripts/make_agent.py
+++ scripts/make_agent.py
@@ -1,33 +1,4 @@
-'''
-from algorithms.baselines import (
-    RandomAgent,
-    FrozenLakeHeuristicAgent,
-    GridWorldHeuristicAgent,
-    CartPoleHeuristicAgent,
-)
-
-def make_agent(agent_name, env_name, env):
-    agent_name = agent_name.lower()
-    env_name = env_name.lower()
-
-    if agent_name == "random":
-        return RandomAgent(env.action_space)
-
-    if env_name == "frozenlake":
-        if agent_name == "heuristic":
-            return FrozenLakeHeuristicAgent(env.action_space)
-
-    if env_name == "gridworld":
-        if agent_name == "heuristic":
-            return GridWorldHeuristicAgent(env.action_space, goal_pos=env.goal_pos)
-
-    if env_name == "cartpole":
-        if agent_name == "heuristic":
-            return CartPoleHeuristicAgent(env.action_space)
-
-    raise ValueError(f"Unsupported agent '{agent_name}' for env '{env_name}'")
-'''
-#when using pretrained model, it may look like this?
+#when using pretrained model, it may look like this? only used by evaluate.py
 from algorithms.baselines import (
     RandomAgent,
     FrozenLakeHeuristicAgent,
